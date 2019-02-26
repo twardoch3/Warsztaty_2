367dfc8daa4b263ddc798b3bd2f8b3d706971862
Tomasz Twardoch <ttwardoch3@gmail.com>
2019-02-26 21:00:52 +0100

    test_update_2

--- src/tests.py
+++ src/tests.py
@@ -10,9 +10,10 @@ class Test_1(unittest.TestCase):
 
     def setUp(self):
         print('password_hash')
-        self.password = 'pass1234P'
+        self.password = 'lk9kiuy$yui'
         self.salt1 = 'vf33wWR'
-        print(password_hash(self.password, salt=self.salt1))
+        self.salt2 = 'vf33wWRdsdsdsdswewdel45453433ertrteterwee'
+        #print(password_hash(self.password, salt=self.salt2))
 
     def test_salt(self):
         self.assertEqual(len(generate_salt()), 16)
@@ -20,21 +21,26 @@ class Test_1(unittest.TestCase):
         self.assertNotIn(',', generate_salt())
 
     def pswd_hash_salt_none(self):
-        self.assertGreater(len(password_hash(self.password)), 16)
+        self.assertEqual(len(password_hash(self.password)), 16 + 64)
         self.assertNotIn('?', password_hash(self.password))
         self.assertNotIn('.', password_hash(self.password))
         self.assertNotIn('=', password_hash(self.password))
 
-    def pswd_hash_salt(self):
-        self.assertGreater(len(password_hash(self.password, )), 16)
+    def pswd_hash_salt_lt_16(self):
+        self.assertEqual(len(password_hash(self.password, )), 16 + 64)
         self.assertNotIn('?', password_hash(self.password, salt=self.salt1))
         self.assertNotIn('-', password_hash(self.password, salt=self.salt1))
         self.assertNotIn(';', password_hash(self.password, salt=self.salt1))
         self.assertEqual(password_hash(self.password, salt=self.salt1)[0:4],'vf33')
         self.assertEqual(password_hash(self.password, salt=self.salt1)[9:13], 'aaaa')
 
-
-
+    def pswd_hash_salt_gt_16(self):
+        self.assertEqual(len(password_hash(self.password, )), 16 + 64)
+        self.assertNotIn('?', password_hash(self.password, salt=self.salt2))
+        self.assertNotIn('-', password_hash(self.password, salt=self.salt2))
+        self.assertNotIn(';', password_hash(self.password, salt=self.salt2))
+        self.assertEqual(password_hash(self.password, salt=self.salt1)[15],self.salt2[15])
+        self.assertNotEqual(password_hash(self.password, salt=self.salt1)[17:29], self.salt2[17:21] )
 
 
 if __name__ == '__main__':

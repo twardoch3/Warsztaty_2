90dacc8a32dea7bbb7695f25ac12f9e98695e074
Tomasz Twardoch <ttwardoch3@gmail.com>
2019-02-26 21:36:37 +0100

    tests_correction

--- src/tests.py
+++ src/tests.py
@@ -9,7 +9,6 @@ from clcrypto import generate_salt, password_hash
 class Test_1(unittest.TestCase):
 
     def setUp(self):
-        print('password_hash')
         self.password = 'lk9kiuy$yui'
         self.salt1 = 'vf33wWR'
         self.salt2 = 'vf33wWRdsdsdsdswewdel45453433ertrteterwee'
@@ -20,27 +19,29 @@ class Test_1(unittest.TestCase):
         self.assertNotIn('?', generate_salt())
         self.assertNotIn(',', generate_salt())
 
-    def pswd_hash_salt_none(self):
+    def test_pswd_hash_salt_none(self):
         self.assertEqual(len(password_hash(self.password)), 16 + 64)
         self.assertNotIn('?', password_hash(self.password))
         self.assertNotIn('.', password_hash(self.password))
         self.assertNotIn('=', password_hash(self.password))
 
-    def pswd_hash_salt_lt_16(self):
+    def test_pswd_hash_salt_lt_16(self):
         self.assertEqual(len(password_hash(self.password, )), 16 + 64)
         self.assertNotIn('?', password_hash(self.password, salt=self.salt1))
         self.assertNotIn('-', password_hash(self.password, salt=self.salt1))
         self.assertNotIn(';', password_hash(self.password, salt=self.salt1))
         self.assertEqual(password_hash(self.password, salt=self.salt1)[0:4],'vf33')
-        self.assertEqual(password_hash(self.password, salt=self.salt1)[9:13], 'aaaa')
+        self.assertEqual(password_hash(self.password, salt=self.salt1)[9:16], 'aaaaaaa')
 
-    def pswd_hash_salt_gt_16(self):
+    def test_pswd_hash_salt_gt_16(self):
+        l = len(self.salt2)
         self.assertEqual(len(password_hash(self.password, )), 16 + 64)
         self.assertNotIn('?', password_hash(self.password, salt=self.salt2))
         self.assertNotIn('-', password_hash(self.password, salt=self.salt2))
         self.assertNotIn(';', password_hash(self.password, salt=self.salt2))
-        self.assertEqual(password_hash(self.password, salt=self.salt1)[15],self.salt2[15])
-        self.assertNotEqual(password_hash(self.password, salt=self.salt1)[17:29], self.salt2[17:21] )
+        self.assertEqual(password_hash(self.password, salt=self.salt2)[10:15],self.salt2[10:15])
+        self.assertNotEqual(password_hash(self.password, salt=self.salt2)[16:l], self.salt2[16:l])
+
 
 
 if __name__ == '__main__':

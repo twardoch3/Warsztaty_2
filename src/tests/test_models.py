d26c70f08646017c12aa1eebca00174322db074c
Tomasz Twardoch <ttwardoch3@gmail.com>
2019-02-27 18:12:58 +0100

    user_hash_psw_test

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -2,7 +2,6 @@ import unittest
 from models import User
 
 
-
 class Test_2(unittest.TestCase):
 
     @classmethod
@@ -11,6 +10,16 @@ class Test_2(unittest.TestCase):
 
     def test_id(self):
         self.assertEqual(self.user.id, -1)
+
+    def test_pwd_hash(self):
+        hash1 = 'er445aaaaaaaaaaa2e15685bc7def4f2aa8da56e7a5e47f62a9d636fa8ea175abf9eb13c427bf336'
+        self.user.hashed_password = {'password': 'pas23', 'salt': 'er445'}
+        self.assertEqual(self.user.hashed_password, hash1)
+        hash2 = '666777DEFGaa88990f5bd282ac89105f24e6ba85e1eb4c3ed417aecd5d223fc753a9d3ab6ec87178'
+        self.user.hashed_password = {'password': 'pass1', 'salt': hash2[:16]}
+        self.assertEqual(self.user.hashed_password, hash2)
+
+
 
 
 
